6c5272ba06a64fe3f188a7b0810ac3011f8cdd09
Louis Alamo <louisgerardoserranoalamo@gmail.com>
2025-07-26 01:09:11 -0600

    Agregar pruebas para la clase Tokenizador y definir patrones de tokenización

---
+++ src/test/testTokenizer.py
@@ -0,0 +1,21 @@
+from src.util.Tokenizador import Tokenizador
+
+
+patrones = [
+    r'\d+\.[a-zA-Z_][a-zA-Z0-9_]*',       # 3.14hola
+    r'\d+[a-zA-Z_][a-zA-Z0-9_]*',         # 3hola
+    r'\d+(\.\d+){2,}',                    # 3.14.15
+    r'\d+\.\d+',                          # 3.14
+    r'\d+\.',                             # 8.
+    r'[a-zA-Z_][a-zA-Z0-9_]*',            # identificadores
+    r'\d+',                               # enteros
+    r'"[^"]*"',                           # cadenas entre comillas
+    r'[,.;:(){}\[\]\+\-\*/=<>!?#%&|@^~]', # delimitadores
+    r'\s+'                                # espacios
+]
+
+codigo = 'inicio x = 3.14; fin'
+tokens = Tokenizador.obtener_tokens_del_codigo(codigo, patrones)
+
+print(tokens)
+# [['inicio', 'x', '=', '3.14', ';', 'fin']]

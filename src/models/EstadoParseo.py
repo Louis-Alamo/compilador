e79629886ab64c35b070c495eca44ab360adbabb
Louis Alamo <louisgerardoserranoalamo@gmail.com>
2025-07-26 15:41:03 -0600

    Agregar métodos para gestionar el estado y el historial en la clase EstadoParseo

--- src/models/EstadoParseo.py
+++ src/models/EstadoParseo.py
@@ -8,9 +8,49 @@ class EstadoParseo:
         self.pasos = 0
 
         self.estado = "n"
-        self.indice = 1
-        self.sentencia_analizada = ""
-        self.sentencia_actual = "#"
+        self.indice = 0
+        self.sentencia_analizada = []
+        self.sentencia_actual = ['inicio', '#']
+
+    def agregar_historial(self, regla):
+        self.pasos += 1
+        self.historial.append({
+            "estado": self.estado,
+            "indice": self.indice,
+            "sentencia_analizada": self.sentencia_analizada,
+            "sentencia_actual": self.sentencia_actual,
+            "pasos": self.pasos,
+            "Regla": regla
+        })
+
+    def set_estado_(self, estado):
+        self.estado = estado
+
+    def agregar_token_a_sentencia_analizada(self, token):
+        self.sentencia_analizada.append(token)
+
+    def eliminar_token_a_sentencia_analizada(self):
+        if len(self.sentencia_actual) > 1:
+            self.sentencia_actual.pop()
+
+    def eliminar_token_a_sentencia_actual(self):
+        if len(self.sentencia_actual) > 1:
+            self.sentencia_actual.pop(0)
+
+    def agregar_token_a_sentencia_actual(self, token):
+        if token not in self.sentencia_actual:
+            self.sentencia_actual.insert(0, token)
+
+    def incrementar_indice(self):
+        self.indice += 1
+
+    def decrementar_indice(self):
+        if self.indice > 1:
+            self.indice -= 1
+
+    #============Metdos Getters================
+    def get_historial(self):
+        return self.historial
 
     def get_informacion_estado(self):
         return {
@@ -21,13 +61,14 @@ class EstadoParseo:
             "pasos": self.pasos
         }
 
-    def agregar_historial(self, estado, indice, sentencia_analizada, sentencia_actual, regla):
-        self.pasos += 1
-        self.historial.append({
-            "estado": estado,
-            "indice": indice,
-            "sentencia_analizada": sentencia_analizada,
-            "sentencia_actual": sentencia_actual,
-            "pasos": self.pasos,
-            "Regla": regla
-        })
+    def get_elemento_sentencia_actual(self, apuntador=0):
+        if apuntador < len(self.sentencia_actual):
+            return self.sentencia_actual[apuntador]
+        else:
+            return None
+
+    def get_indice(self):
+        return self.indice
+
+    def get_estado(self):
+        return self.estado
